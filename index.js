249fd220d15d57ab80201a636f210f33f9ad44c3
franciscoleiva <52587671+franciscoleiva@users.noreply.github.com>
2024-12-14 16:23:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const axios = require('axios');
 
 const url = process.env.CONSUMER1
-const sl1 = process.env.SOL1
 
-const dataD = {
-  solicitud: sl1
-};
-
-axios.post(url, dataD)
+axios.get(url)
   .then(response => {
     console.log('Respuesta:', response.data);
   })
